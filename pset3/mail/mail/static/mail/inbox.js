5449f92c66f80d0f96bcf2ec6cd9423a3f8f4dfb
Ray Wu <raywupharmd@outlook.com>
2020-08-06 15:22:26 -0400

    complete view email requirements to show the email’s sender, recipients, subject, timestamp, and body. also marks email as read with PUT request to server once an email is selected.

--- pset3/mail/mail/static/mail/inbox.js
+++ pset3/mail/mail/static/mail/inbox.js
@@ -8,6 +8,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // By default, load the inbox
   load_mailbox('inbox');
+
+  // Add event listener on each email div
 });
 
 function compose_email() {
@@ -39,8 +41,8 @@ function load_mailbox(mailbox) {
   fetch(`/emails/${mailbox}`)
   .then(response => response.json())
   .then(emails => {
-    // iterate through each email
     // console.log (emails)
+    // iterate through each email
     for (email in emails) {
       console.log('email', emails[email]);
       // Create card div
@@ -50,45 +52,158 @@ function load_mailbox(mailbox) {
       card.classList.add('d-flex');
       card.classList.add('bd-highlight'); 
       card.classList.add('justify-content-start'); 
+      
+      // If the email has been read, it should appear with a gray background.
+      if (emails[email]['read']) {
+        card.classList.add('bg-secondary');
+        // card.classList.add('text-white');
+      }
+      
       // Create div for our subject
       const subject = document.createElement('div');
-      subject.classList.add('p-2')
-      // subject.classList.add('flex-fill')
-      subject.classList.add('flex-grow-1')
-      subject.classList.add('bd-highlight')
+      subject.classList.add('p-2');
+      subject.classList.add('flex-grow-1');
+      subject.classList.add('bd-highlight');
       subject.innerHTML = emails[email]['subject'];
- 
-      // const body = document.createElement('div');
-      // body.classList.add('p-2')
-      // body.classList.add('flex-fill')
-      // body.classList.add('bd-highlight')
-      // body.innerHTML = emails[email]['body'];
 
       // Create div for our sender
       const sender = document.createElement('div');
-      sender.classList.add('p-2')
-      // sender.classList.add('flex-fill')
-      sender.classList.add('bd-highlight')
+      sender.classList.add('p-2');
+      sender.classList.add('bd-highlight');
       const strongsender = document.createElement('strong');
       strongsender.innerHTML = emails[email]['sender'];
-      sender.append(strongsender)
+      sender.append(strongsender);
 
       // Create div for our timestamp
       const timestamp = document.createElement('div');
-      timestamp.classList.add('p-2')
-      // timestamp.classList.add('flex-fill')
-      timestamp.classList.add('bd-highlight')
-      timestamp.classList.add('ml-auto')
+      timestamp.classList.add('p-2');
+      timestamp.classList.add('bd-highlight');
+      timestamp.classList.add('ml-auto');
       timestamp.innerHTML = emails[email]['timestamp'];
 
       // Add all elements to the card
       card.append(sender);
       card.append(subject);
       card.append(timestamp);
-      // card.append(body);
 
       // Add emails to the emails-view div 
       emails_div.append(card);
+
+      // add event listener to email div
+      card.addEventListener('click', function() {
+        fetch(`/emails/${emails[email]['id']}`)
+        .then(response => response.json())
+        .then(email => {
+            console.log(email);
+
+            // Update the email to read = true
+            if (email['read'] === false) {
+              fetch(`/emails/${email['id']}`, {
+                method: 'PUT',
+                body: JSON.stringify({
+                    read: true
+                })
+              })
+            }
+            // console.log('read? ', email);
+
+            // clear our emails view
+            document.querySelector('#emails-view').innerHTML = '';
+            
+            // show the email’s sender, recipients, subject, timestamp, and body.
+            const div = document.createElement('div');
+
+            // Email Sender
+            const email_sender = document.createElement('p');
+            const email_from = document.createElement('strong');
+            email_from.innerHTML = 'From: ';
+            email_sender.append(email_from);
+            email_sender.append(email['sender']);
+            email_sender.classList.add('m-0');
+
+            // Email Recipients
+            const email_recipients = document.createElement('p');
+            const email_to = document.createElement('strong');
+            email_to.innerHTML = 'To: ';
+            email_recipients.append(email_to);
+            email_recipients.append(email['recipients']);
+            email_recipients.classList.add('m-0');
+
+            // Email Subject
+            const email_subject_info = document.createElement('p');
+            const email_subject= document.createElement('strong');
+            email_subject.innerHTML = 'Subject: ';
+            email_subject_info.append(email_subject);
+            email_subject_info.append(email['subject']);
+            email_subject_info.classList.add('m-0');
+
+            // Email Timestamp
+            const email_timestamp_info = document.createElement('p');
+            const email_timestamp= document.createElement('strong');
+            email_timestamp.innerHTML = 'Timestamp: ';
+            email_timestamp_info.append(email_timestamp);
+            email_timestamp_info.append(email['timestamp']);
+            email_timestamp_info.classList.add('m-0');
+
+            // Add an <hr>
+            const hr = document.createElement('hr');
+
+            // Email Body
+            const body = document.createElement('div');
+            body.innerHTML = email['body'];
+
+
+            // Reply form and button
+
+            //create a form
+            const btn = document.createElement('button');
+            btn.classList.add('btn');
+            btn.classList.add('btn-sm');
+            btn.classList.add('btn-outline-primary');
+            btn.innerHTML = 'Reply';
+
+            // Example from https://stackoverflow.com/questions/3297143/dynamically-create-a-html-form-with-javascript
+            // var f = document.createElement("form");
+            // f.setAttribute('method',"post");
+            // f.setAttribute('action',"submit.php");
+
+            // //create input element
+            // var i = document.createElement("input");
+            // i.type = "text";
+            // i.name = "user_name";
+            // i.id = "user_name1";
+
+            // //create a checkbox
+            // var c = document.createElement("input");
+            // c.type = "checkbox";
+            // c.id = "checkbox1";
+            // c.name = "check1";
+
+            // //create a button
+            // var s = document.createElement("input");
+            // s.type = "submit";
+            // s.value = "Submit";
+
+            // // add all elements to the form
+            // f.appendChild(i);
+            // f.appendChild(c);
+            // f.appendChild(s);
+
+            // // add the form inside the body
+            // $("body").append(f);   //using jQuery or
+            // document.getElementsByTagName('body')[0].appendChild(f); //pure javascript
+
+            // Append elements to the new div
+            div.append(email_sender);
+            div.append(email_recipients);
+            div.append(email_subject_info);
+            div.append(email_timestamp_info);
+            div.append(btn);
+            div.append(hr);
+            div.append(body);
+            emails_div.append(div);
+      });
+      });
     }
   });
 }
