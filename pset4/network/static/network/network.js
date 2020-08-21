f322f55890222d75970c4c911f7c6ee867d3514b
Ray Wu <raywupharmd@outlook.com>
2020-08-20 21:00:00 -0400

    added javascript to replace the post the user is editting with a form. TODO async update for the post in the database

--- pset4/network/static/network/network.js
+++ pset4/network/static/network/network.js
@@ -1,5 +1,62 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-    // TODO
-    console.log("loaded")
+    // const cards = document.querySelectorAll('.edit-link');
+    const edit_buttons = document.querySelectorAll('.edit-link');
+    
+    edit_buttons.forEach(button => button.addEventListener('click', () => {
+        createForm(button['parentElement']['parentElement']);
+        return false;
+        })
+    )
 }) 
+
+function createForm(parentElem) {
+    const body = parentElem['children'][1];
+    console.log(body);
+    const old_body_content = body['children'][1].innerHTML;
+    body.innerHTML = '';
+
+    const form = document.createElement('form');
+    form.classList.add('mb-3');
+
+    const inputElem = document.createElement('input');
+    inputElem.type = 'hidden';
+    inputElem.name = 'csrfmiddlewaretoken';
+    inputElem.value = CSRF_TOKEN;
+    form.appendChild(inputElem);
+
+    const div = document.createElement('div');
+    div.classList.add('form-group');
+
+    const label = document.createElement('label');
+    label.innerHTML = 'Edit Post';
+    label.htmlFor = 'postContent';
+
+    const textarea = document.createElement('textarea');
+    textarea.classList.add('form-control');
+    textarea.id = 'postContent';
+    textarea.name = 'post';
+    textarea.rows = 3;
+    textarea.value = old_body_content;
+
+    div.append(label);
+    div.append(textarea);
+    
+    const input = document.createElement('input');
+    input.classList.add('btn');
+    input.classList.add('btn-primary');
+    input.value = 'Post';
+    input.type = 'submit';
+    
+    form.append(div);
+    form.append(input);
+    form.onsubmit = () => {
+        console.log('submitted!');
+        // TODO need to async update the data
+
+
+        // Stop form from submitting
+        return false;
+    }
+    
+    body.append(form);
+}
--- pset4/network/urls.py
+++ pset4/network/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("following", views.following_view, name="following"),
-    # path("post/<int:post_id>", views.post_view, name="post"),
     path("user/<int:user_id>", views.user_view, name="user"),
     path("register", views.register, name="register")
 ]
